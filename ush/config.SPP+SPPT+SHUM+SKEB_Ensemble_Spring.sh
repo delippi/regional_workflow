68c3b7261f13b1bc0f539b77b44ff8505b618748
jeff beck <jeff.beck@noaa.gov>
2022-11-18 22:59:50 +0000

    Config files for stochastic physics simulations.

---
+++ ush/config.SPP+SPPT+SHUM+SKEB_Ensemble_Spring.sh
@@ -0,0 +1,76 @@
+MACHINE="hera"
+ACCOUNT="fv3lam"
+EXPT_SUBDIR="SPPT_SKEB_SHUM_SPP_Ensemble_Spring"
+
+USE_CRON_TO_RELAUNCH="TRUE"
+CRON_RELAUNCH_INTVL_MNTS="02"
+
+DO_ENSEMBLE="TRUE"
+NUM_ENS_MEMBERS="10"
+
+COMPILER="intel"
+VERBOSE="TRUE"
+
+RUN_ENVIR="community"
+PREEXISTING_DIR_METHOD="rename"
+
+PREDEF_GRID_NAME="RRFS_CONUScompact_3km"
+
+RUN_TASK_RUN_POST="TRUE"
+
+CCPP_PHYS_SUITE="FV3_HRRR"
+FCST_LEN_HRS="36"
+LBC_SPEC_INTVL_HRS="3"
+
+DT_ATMOS="25"
+
+DATE_FIRST_CYCL="20210511"
+DATE_LAST_CYCL="20210518"
+CYCL_HRS=( "00" )
+
+EXTRN_MDL_NAME_ICS="HRRR"
+EXTRN_MDL_NAME_LBCS="RAP"
+
+QUILTING="TRUE"
+
+WTIME_RUN_FCST="08:00:00"
+
+DO_SPPT="true"
+DO_SKEB="true"
+DO_SHUM="true"
+DO_SPP="true"
+
+MODEL="FV3_HRRR_CONUS_3KM_SPPT_SKEB_SHUM_SPP_ENSEMBLE"
+METPLUS_PATH="/contrib/METplus/METplus-4.1.1"
+MET_INSTALL_DIR="/contrib/met/10.1.1"
+CCPA_OBS_DIR="/scratch2/BMC/fv3lam/HIWT/obs_data/ccpa/proc"
+MRMS_OBS_DIR="/scratch2/BMC/fv3lam/HIWT/obs_data/mrms/proc"
+NDAS_OBS_DIR="/scratch2/BMC/fv3lam/HIWT/obs_data/ndas/proc"
+
+RUN_TASK_MAKE_GRID="TRUE"
+RUN_TASK_MAKE_OROG="TRUE"
+RUN_TASK_MAKE_SFC_CLIMO="TRUE"
+RUN_TASK_GET_OBS_CCPA="FALSE"
+RUN_TASK_GET_OBS_MRMS="FALSE"
+RUN_TASK_GET_OBS_NDAS="FALSE"
+RUN_TASK_VX_GRIDSTAT="TRUE"
+RUN_TASK_VX_POINTSTAT="TRUE"
+RUN_TASK_VX_ENSGRID="TRUE"
+RUN_TASK_VX_ENSPOINT="TRUE"
+
+#
+# Uncomment the following line in order to use user-staged external model 
+# files with locations and names as specified by EXTRN_MDL_SOURCE_BASEDIR_ICS/
+# LBCS and EXTRN_MDL_FILES_ICS/LBCS.
+#
+#USE_USER_STAGED_EXTRN_FILES="TRUE"
+#
+# The following is specifically for Hera.  It will have to be modified
+# if on another platform, using other dates, other external models, etc.
+# Uncomment the following EXTRN_MDL_*_ICS/LBCS only when USE_USER_STAGED_EXTRN_FILES=TRUE
+#
+#EXTRN_MDL_SOURCE_BASEDIR_ICS="/scratch2/BMC/det/UFS_SRW_app/v1p0/model_data/FV3GFS"
+#EXTRN_MDL_FILES_ICS=( "gfs.pgrb2.0p25.f000" )
+#EXTRN_MDL_SOURCE_BASEDIR_LBCS="/path/to/model_data/FV3GFS"
+#EXTRN_MDL_FILES_LBCS=( "gfs.pgrb2.0p25.f006" "gfs.pgrb2.0p25.f012" "gfs.pgrb2.0p25.f018" "gfs.pgrb2.0p25.f024" \
+#                       "gfs.pgrb2.0p25.f030" "gfs.pgrb2.0p25.f036" "gfs.pgrb2.0p25.f042" "gfs.pgrb2.0p25.f048" )
